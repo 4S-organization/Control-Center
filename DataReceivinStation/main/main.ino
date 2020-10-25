8ef38f2636bda6b650d2bc12b53ae7262b335a92
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-10-25 20:07:06 +0500

    added data acquisition from HC-12

--- DataReceivinStation/main/main.ino
+++ DataReceivinStation/main/main.ino
@@ -1,3 +1,21 @@
-void setup() {}
+#include <SoftwareSerial.h>
 
-void loop() {}
+#define RX 8
+#define TX 9
+
+SoftwareSerial HC12(RX,TX);
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);
+}
+
+void loop() {
+  if(HC12.available()){
+    if (HC12.read() == 0xAA){
+      Serial.println('1');
+    } else {
+      Serial.println('0');
+    }
+  }
+}  
